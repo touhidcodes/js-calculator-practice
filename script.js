a01ef7eef6aa55c45fbdd9f00b7734472a1e687a
touhidcodes <touhidcodes@gmail.com>
2022-10-22 18:11:32 +0600

    function cmplt

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function deleteMe() {
-  document.getElementById("show-result").value = "";
+  document.getElementById("output-result").value = "";
 }
 
 function removeMe() {
-  const output = document.getElementById("show-result");
+  const output = document.getElementById("output-result");
   output.value = output.value.slice(0, -1);
 }
 
 function calculator(newValue) {
-  document.getElementById("show-result").value += newValue;
+  document.getElementById("output-result").value += newValue;
+}
+
+function finalResult() {
+  const a = document.getElementById("output-result").value;
+  const b = eval(a);
+  document.getElementById("output-result").value = b;
 }
